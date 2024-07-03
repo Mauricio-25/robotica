8629f26be5cf9a6f2a42a81a57e8ad34744d893f
Mauricio-25 <mau252004@gmail.com>
2024-07-03 18:02:58 -0500

    corregido

--- arduino/code_esp32.ino
+++ arduino/code_esp32.ino
@@ -1,8 +1,16 @@
-#include <WiFi.h>
-#include <WebSocketsClient.h>
-#include <ArduinoJson.h>
 
-#include <Servo.h>
+/*
+
+  A = GARRA
+  B = BRAZO
+  C = CUERPO
+  D = BASE
+  
+
+*/
+
+
+#include <ESP32Servo.h>
 #include <Stepper.h>
 
 Servo servomotor4; //Servomotor GARRA
@@ -11,89 +19,9 @@ Servo servomotor2; //Servomotor CUERPO
 Servo servomotor1; //Servomotor BASE
 Stepper motor(2048, 4, 6, 5, 7);
 
-const char* ssid = "UPN_ESTUDIANTES"; // Nombre wifi
-const char* password = "H@azLoCorrectoUPN"; // Contraseña wifi
-const char* host = "192.168.1.100"; // Dirección IP de tu servidor
-const int port = 3002;
-
-WebSocketsClient webSocket;
-
-void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
-  switch(type) {
-    case WStype_DISCONNECTED:
-      Serial.println("Desconectado!");
-      break;
-    case WStype_CONNECTED:
-      Serial.println("Conectado al servidor!");
-      break;
-    case WStype_TEXT:
-      Serial.printf("Evento recibido: %s\n", payload);
-
-      // Parsear el JSON recibido
-      DynamicJsonDocument doc(1024);
-      deserializeJson(doc, payload);
-      
-      if (doc.containsKey("range_base")) {
-        int valor = doc["range_base"];
-        Serial.printf("Base: %d°\n", valor);
-
-        // Código para controlar la base
-        servomotor1.write(valor);
-        delay(25);
-
-      } else if (doc.containsKey("range_cuerpo")) {
-        int valor = doc["range_cuerpo"];
-        Serial.printf("Cuerpo: %d°\n", valor);
-
-        // Código para controlar el cuerpo
-        servomotor2.write(valor);
-        delay(25);
-
-      } else if (doc.containsKey("range_brazo")) {
-        int valor = doc["range_brazo"];
-        Serial.printf("Brazo: %d°\n", valor);
-
-        // Código para controlar el brazo
-        servomotor3.write(valor);
-        delay(25);
-
-      } else if (doc.containsKey("garra")) {
-        bool valor = doc["garra"];
-        Serial.printf("Toggle Garra: %s\n", valor ? "Abierto" : "Cerrado");
-
-        // Código para controlar la garra
-        if(valor) {
-            servomotor4.write(90);
-        } else {
-            servomotor4.write(0);
-        }
-
-      } else if (doc.containsKey("mover")) {
-        String direction = doc["mover"];
-        Serial.printf("Moviendo carrito hacia: %s\n", direction.c_str());
-
-      } else if (String((char*)payload) == "detener") {
-        Serial.println("Deteniendo carrito");
-      }
-      break;
-  }
-}
-
 void setup() {
 
-  // WIFI
-  Serial.begin(115200);
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Conectando al WiFi...");
-  }
-
-  Serial.println("Conexion existosa al WiFi");
-  webSocket.begin(host, port, "/socket.io/?EIO=4");
-
-  webSocket.onEvent(webSocketEvent);
+  Serial.begin(115200); // Inicia la comunicación serial a 115200 bps
 
   // Servo
   servomotor3.attach(12);
@@ -104,5 +32,58 @@ void setup() {
 }
 
 void loop() {
-  webSocket.loop();
+
+  char primerCaracter;
+  char letra;
+  String datoCompleto;
+  int dato;
+
+  if (Serial.available()) {
+    primerCaracter = Serial.read(); // Lee el primer carácter
+    if (isAlpha(primerCaracter)) {
+        // Es una letra (A-Z o a-z)
+        letra = primerCaracter;
+        datoCompleto = Serial.readString(); // Lee el dato completo
+        dato = datoCompleto.toInt(); // Convierte el dato a entero
+        // Ahora tienes la letra y el dato por separado (letra y dato)
+    }
+
+    if (letra == 'A') {
+
+      Serial.printf("Garra: %s\n", dato ? "Abierto" : "Cerrado");
+
+      // Código para controlar la garra
+      if(valor) {
+        servomotor4.write(90);
+      } else {
+        servomotor4.write(0);
+      }
+
+
+    } else if (letra == 'B') {
+
+      Serial.printf("Brazo: %d°\n", dato);
+
+      // Código para controlar el brazo
+      servomotor3.write(dato);
+      delay(25);
+
+    } else if (letra == 'C') {
+
+      Serial.printf("Cuerpo: %d°\n", dato);
+
+      // Código para controlar el cuerpo
+      servomotor2.write(dato);
+      delay(25);
+
+    } else if (letra == 'D') {
+
+      Serial.printf("Base: %d°\n", dato);
+
+      // Código para controlar la base
+      servomotor1.write(dato);
+      delay(25);
+
+    }
+  }
 }
--- public/src/js/script.js
+++ public/src/js/script.js
@@ -1,28 +1,9 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-  const socket = io();
+  const socket = io('http://192.168.1.39:3002');
 
-  /*
-const form = document.getElementById("form");
-const input = document.getElementById("input");
-const messages = document.getElementById("messages");
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  if (input.value) {
-    socket.emit("chat message", input.value);
-    input.value = "";
-  }
-});
-
-socket.on("chat message", (msg) => {
-  const item = document.createElement("li");
-  item.textContent = msg;
-  messages.appendChild(item);
-  window.scrollTo(0, document.body.scrollHeight);
-});
-
-*/
+  const puertoSerial = new SerialPort('COM3', { baudRate: 115200 });
+  
   // ! CORREGIR EL MOVER LA PANTALLA CON LOS INPUTS
 
   const rangeInputs = document.querySelectorAll('input[type="range"]');
@@ -139,6 +120,7 @@ socket.on("chat message", (msg) => {
 
   const startMoving = (direction) => {
     socket.emit("mover", direction);
+
     intervalId = setInterval(() => {
       socket.emit("mover", direction);
       console.log(`El carrito se mueve a la direccion: ${direction}`);
@@ -263,6 +245,21 @@ socket.on("chat message", (msg) => {
 
         socket.emit(`${dataSocket}`, valor);
 
+        let dato;
+
+        if (dataSocket == "range_brazo") {
+          dato = `B${valor}`;
+          puertoSerial.write(dato.toString());
+
+        } else if (dataSocket == "range_cuerpo") {
+          dato = `C${valor}`;
+          puertoSerial.write(dato.toString());
+
+        } else if (dataSocket == "range_base") {
+          dato = `D${valor}`;
+          puertoSerial.write(dato.toString());
+        }
+
       })
     }
   }
@@ -273,6 +270,7 @@ socket.on("chat message", (msg) => {
         let texto = buttons[i].parentNode.querySelector(".texto");
         let circulo = buttons[i].children[0];
         let dataSocket = buttons[i].getAttribute('data-socket');
+        let dato;
 
         if (texto.innerHTML == "Abierto") {
           texto.innerHTML = `Cerrado`;
@@ -282,6 +280,10 @@ socket.on("chat message", (msg) => {
           circulo.classList.remove("left-[23px]");
 
           socket.emit(`${dataSocket}`, false);
+
+          dato = `A0`;
+          puertoSerial.write(dato.toString());
+
         } 
 
         else {
@@ -292,6 +294,9 @@ socket.on("chat message", (msg) => {
           circulo.classList.add("left-[23px]");
 
           socket.emit(`${dataSocket}`, true);
+
+          dato = `A1`;
+          puertoSerial.write(dato.toString());
         }
       })
     }
@@ -384,60 +389,69 @@ socket.on("chat message", (msg) => {
 
   // ! CONECTAR CON BLUETOOTH 
 
-  let bluetoothDevice;
+  /*
+  • chrome://flags/
+  • Experimental Web Platform features ENABLED
+  */
 
-  async function connect() {
+
+  async function conectarBluetooth() {
+    
+
+    if (!navigator.bluetooth) {
+      console.log('API de Bluetooth no soportada en este navegador.');
+      return;
+    }
+  
     try {
-      // Solicitar permiso para acceder al dispositivo Bluetooth
-      bluetoothDevice = await navigator.bluetooth.requestDevice({
-        filters: [{ services: ['serial'] }] // Filtro para el servicio serial Bluetooth
+      const device = await navigator.bluetooth.requestDevice({
+        acceptAllDevices: true,
+        optionalServices: ['battery_service']
       });
   
-      // Conectar al dispositivo Bluetooth
-      const server = await bluetoothDevice.gatt.connect();
+      console.log('Dispositivo seleccionado:', device.name);
   
-      // Obtener el servicio serial
-      const service = await server.getPrimaryService('serial');
+      // Conectar al dispositivo
+      const server = await device.gatt.connect();
+      console.log('Conectado al dispositivo');
   
-      // Obtener la característica para escribir datos
-      const characteristic = await service.getCharacteristic('write');
+      // Enumerar servicios disponibles
+      const services = await server.getPrimaryServices();
+      console.log('Servicios disponibles:', services.map(service => service.uuid));
   
-      // Función para enviar un comando al Arduino
-      function enviarDireccion(direction) {
-        const encoder = new TextEncoder();
-        characteristic.writeValue(encoder.encode(direction));
+      // Verificar si el servicio de batería está disponible
+      const batteryService = services.find(service => service.uuid === 'battery_service' || service.uuid === '0000180f-0000-1000-8000-00805f9b34fb');
+  
+      if (!batteryService) {
+        console.log('El servicio de batería no está disponible en este dispositivo.');
+        return;
       }
   
-      // Ejemplo de enviar comandos al Arduino
-      enviarDireccion('up'); // Avanzar
-      enviarDireccion('left');    // Girar a la izquierda
+      // Obtener característica del servicio de batería
+      const characteristic = await batteryService.getCharacteristic('battery_level');
   
-      // Escuchar datos recibidos del Arduino
-      /*
-      characteristic.addEventListener('characteristicvaluechanged', (event) => {
-        const decoder = new TextDecoder();
-        console.log('Datos recibidos:', decoder.decode(event.target.value));
-      });
-      */
-  
-      // Habilitar notificaciones de la característica para recibir datos
-      //await characteristic.startNotifications();
-
-      btnConectar.style.background = `#9FDCFF`;
-      btnConectar.classList.remove("cursor-pointer");
-      btnConectar.classList.remove("hover:bg-slate-100");
-
-      socket.emit("bluetooth", true);
+      // Leer valor de la característica
+      const value = await characteristic.readValue();
+      let batteryLevel = value.getUint8(0);
+      console.log('Nivel de batería:', batteryLevel + '%');
   
     } catch (error) {
-      console.error('Error al conectar con el dispositivo Bluetooth:', error);
+      console.log('Error:', error);
     }
+
+
+
+
+
   }
+
+
+
 
   // * Event listener para el botón de conexión
   const btnConectar = document.getElementById('conectar');
   if(btnConectar) {
-    btnConectar.addEventListener('click', connect);
+    btnConectar.addEventListener('click', conectarBluetooth);
   }
   
 
